91cc47751bf1dbe4acbfd647d5e3d56cf7684e4e
kokila <kokilarajendran98@gmail.com>
2023-08-14 10:26:38 +0530

    Header Added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+export const Header = () => {
+   return(
+    <div className="header">
+      <img className="image" src="sync.png" alt="sync"/>
+       <h1>Sync & Done</h1>
+      <img src="check.png" alt="" className='image'/>
+    </div>
+   )
+}
