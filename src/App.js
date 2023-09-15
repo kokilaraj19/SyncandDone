bb553f61f608f370dbb408b9aee05350e3ade909
kokila <kokilarajendran98@gmail.com>
2023-09-15 12:59:50 +0530

    App completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import {Header} from './Components/Header/Header'
 import {Form} from './Components/Form/form'
 import {Footer} from './Components/Footer/Footer'
-import './App.css';
 import { useState } from 'react';
-import {MdDelete} from 'react-icons/md'
+import { TodoList } from './Components/To-do-List/To-do-List';
+import './App.css';
 
 function App() {
   const [list,setList] = useState([]);
@@ -18,56 +18,6 @@ function App() {
   );
 }
 
-function TodoList(props){
-  const {list,setList} = props;
-  const DeleteHandler = (id) =>{
-   setList(list.filter((item) => item.id !== id))
-  }
-  const isDoneHandler = ({id}) =>{
-    console.log(id)
-    setList(list.map((item) => item.id===id? ({...item,isDone: !item.isDone}): item))
-  }
-  const sortHandler = (event) =>{
-     const donearr=list.filter((it) => it.isDone);
-     const notdonearr = list.filter((it) => !it.isDone)
-     event.target.value === "Completed" ? setList([...donearr,...notdonearr]): setList([...notdonearr,...donearr]);
-  }
-  const clearHandler = () =>{
-    setList([])
-    console.log("inside")
-  }
-  return (
-    <div className='List-container'> 
-      <div className='empty-container'>   
-         {list.length>0 ? list.map((item) => <Item  key={item.id} id={item.id} date={item.date} activity={item.activity} isDone= {item.isDone} isdoneHandler = {isDoneHandler} deleteHandler={DeleteHandler }/>): ""}
-        
-
-      </div>
-       <section className='sorting-container'>
-            <select onChange={sortHandler} id='choices'>
-               <option>Filter By</option>
-               <option>Completed</option>
-               <option>Not completed</option>
-            </select>
-            <button onClick={clearHandler}>Clear List</button>
-        </section>
-    </div>
-
-    )
-}
-
-function Item(props){
-  const {id,activity,isDone,deleteHandler,isdoneHandler} = props;
-  // console.log(isDone)
-  
-  return (
-    <div key={id} className='item-flex item-font'>
-      <input type="checkbox" value={isDone} onChange={() => isdoneHandler({id})}/>
-      <p style ={props.isDone ? {textDecoration: "line-through"}: {}}>{activity}</p>
-      <MdDelete onClick={() => deleteHandler(id)}/>
-    </div>
-  );
-}
 
 
 export default App;
--- src/Components/To-do-List/To-do-List.jsx
+++ src/Components/To-do-List/To-do-List.jsx
@@ -1,7 +1,53 @@
 import './To-do-List.css'
+import {MdDelete} from 'react-icons/md'
 
-export const TodoList= () =>{
-    return <div className='To-dos'>
+export function TodoList(props){
+  const {list,setList} = props;
+  const DeleteHandler = (id) =>{
+   setList(list.filter((item) => item.id !== id))
+  }
+  const isDoneHandler = ({id}) =>{
+    console.log(id)
+    setList(list.map((item) => item.id===id? ({...item,isDone: !item.isDone}): item))
+  }
+  const sortHandler = (event) =>{
+     const donearr=list.filter((it) => it.isDone);
+     const notdonearr = list.filter((it) => !it.isDone)
+     event.target.value === "Completed" ? setList([...donearr,...notdonearr]): setList([...notdonearr,...donearr]);
+  }
+  const clearHandler = () =>{
+    setList([])
+    console.log("inside")
+  }
+  return (
+    <div className='List-container'> 
+      <div className='empty-container'>   
+         {list.length>0 ? list.map((item) => <Item  key={item.id} id={item.id} date={item.date} activity={item.activity} isDone= {item.isDone} isdoneHandler = {isDoneHandler} deleteHandler={DeleteHandler }/>): ""}
         
+
+      </div>
+       <section className='sorting-container'>
+            <select onChange={sortHandler} id='choices'>
+               <option>Filter By</option>
+               <option>Completed</option>
+               <option>Not completed</option>
+            </select>
+            <button onClick={clearHandler}>Clear List</button>
+        </section>
     </div>
+
+    )
+}
+
+function Item(props){
+  const {id,activity,isDone,deleteHandler,isdoneHandler} = props;
+  // console.log(isDone)
+  
+  return (
+    <div key={id} className='item-flex item-font'>
+      <input type="checkbox" value={isDone} onChange={() => isdoneHandler({id})}/>
+      <p style ={ props.isDone? {textDecoration: "line-through",textTransform: "capitalize"}: {textTransform: "capitalize"}}>{activity}</p>
+      <MdDelete onClick={() => deleteHandler(id)}/>
+    </div>
+  );
 }
